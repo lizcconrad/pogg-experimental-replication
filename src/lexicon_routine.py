30333d6ba9e7d0c4e8f33fc7ef5fbeaf67169503
Elizabeth Conrad <lizcconrad@gmail.com>
2026-04-23 15:27:58 -0700

    update lexicon routine to enable reading templates from multiple files

--- src/lexicon_routine.py
+++ src/lexicon_routine.py
@@ -13,9 +13,10 @@ parser.add_argument("composition_config",  type=str, help="path to the config wh
 parser.add_argument("experiment_config",  type=str, help="path to the config which specifies experiments")
 
 parser.add_argument("-a", "--auto_filling",  action="store_true", help="attempt to auto-fill lexicon if argument is provided")
-parser.add_argument("-t", "--lexicon_templates",  type=str, help="path to the file of lexicon entry templates")
+parser.add_argument("-t", "--lexicon_template_files",  nargs="+", type=str, help="list of paths to files of lexicon entry templates")
 parser.add_argument("-s", "--string_processing_fxn",  type=str, help="name of string processing function to use from string_processing.py")
 parser.add_argument("-f", "--find_new_templates",  action="store_true", help="find new templates from lexicon to template file if argument is provided")
+parser.add_argument("-d", "--dump_file",  type=str, help="file to dump new templates to; can be one of the files templates are being pulled from")
 
 args = parser.parse_args()
 
@@ -25,15 +26,17 @@ experiment_config = args.experiment_config
 auto_filling = args.auto_filling
 find_new_templates = args.find_new_templates
 
-if auto_filling:
-    if args.lexicon_templates is None:
+if auto_filling or find_new_templates:
+    if args.lexicon_template_files is None:
         raise Exception("When auto_filling, -t (--lexicon_templates) must be provided")
     if args.string_processing_fxn is None:
-        raise Exception("When auto_filling, -t (--string_processing_fxn) must be provided")
+        raise Exception("When auto_filling, -s (--string_processing_fxn) must be provided")
+    if not find_new_templates:
+        if args.dump_file is None:
+            raise Exception("When finding new templates, -d (--dump_file) must be provided")
 
-    lexicon_templates = args.lexicon_templates
     str_processing_fxn = getattr(string_processing, args.string_processing_fxn)
-    lexicon_auto_filler = POGGLexiconAutoFiller(composition_config, lexicon_templates)
+    lexicon_auto_filler = POGGLexiconAutoFiller(composition_config, args.lexicon_template_files, args.dump_file)
 
 
 
